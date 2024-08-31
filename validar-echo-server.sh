65f775df6640092702be3ea260e788231dad2893
alejofabregas <afabregas@fi.uba.ar>
2024-08-31 18:01:45 -0300

    Comparación y ejecución del server
    Se le agrega la comparación de las strings del mensaje y el resultado con la salida esperada para cada caso.
    Se ejecuta el server en el script para no tener que hacerlo aparte.
    Se agregan las variables de dirección y puerto del server.

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+
+DIRECCION_SERVER="server"
+PUERTO_SERVER="12345"
 MENSAJE="HOLA MUNDO"
+
+make docker-compose-up
 
 docker run --name client-netcat -it --network tp0_testing_net ubuntu bash -c "apt-get update && apt-get install --assume-yes netcat-openbsd"
 docker start client-netcat
 
-RESPUESTA=$(docker exec -it client-netcat bash -c "echo HOLA MUNDO | netcat server 12345")
+RESPUESTA=$(docker exec client-netcat bash -c "echo $MENSAJE | netcat $DIRECCION_SERVER $PUERTO_SERVER")
 
 docker stop client-netcat
 docker rm client-netcat
 
-echo "Mensaje enviado: $MENSAJE"
-echo "Respuesta recibida: $RESPUESTA"
+make docker-compose-down
+
+if [ "$RESPUESTA" = "$MENSAJE" ];
+then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
