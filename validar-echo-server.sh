05cbc0358866b04cf3005b4dc1630bcdf03e6362
alejofabregas <afabregas@fi.uba.ar>
2024-09-03 00:19:14 -0300

    Fix 2 test ej3

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-DIRECCION_SERVER=$(grep SERVER_IP server/config.ini | cut -d ' ' -f 3)
-PUERTO_SERVER=$(grep SERVER_PORT server/config.ini | cut -d ' ' -f 3)
+DIRECCION_SERVER=$(awk -F' = ' '/SERVER_IP/ {print $2}' server/config.ini | tr -d '[:space:]')
+PUERTO_SERVER=$(awk -F' = ' '/SERVER_PORT/ {print $2}' server/config.ini | tr -d '[:space:]')
 MENSAJE="HOLA MUNDO"
 
 # make docker-compose-up
 
-docker run --name client-netcat -it --network tp0_testing_net ubuntu bash -c "apt-get update && apt-get install --assume-yes netcat-openbsd"
-docker start client-netcat
-
-RESPUESTA=$(docker exec client-netcat bash -c "echo $MENSAJE | netcat $DIRECCION_SERVER $PUERTO_SERVER")
-
-docker stop client-netcat
-docker rm client-netcat
+RESPUESTA=$(docker run --rm --name client-netcat --network tp0_testing_net busybox sh -c "echo $MENSAJE | nc $DIRECCION_SERVER $PUERTO_SERVER")
 
 # make docker-compose-down
 
