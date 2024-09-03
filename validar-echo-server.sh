1d0d8d63b95552f9b05b395ae4003dee52358f74
alejofabregas <afabregas@fi.uba.ar>
2024-09-02 23:13:21 -0300

    Fix test ej3

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -4,7 +4,7 @@ DIRECCION_SERVER=$(grep SERVER_IP server/config.ini | cut -d ' ' -f 3)
 PUERTO_SERVER=$(grep SERVER_PORT server/config.ini | cut -d ' ' -f 3)
 MENSAJE="HOLA MUNDO"
 
-make docker-compose-up
+# make docker-compose-up
 
 docker run --name client-netcat -it --network tp0_testing_net ubuntu bash -c "apt-get update && apt-get install --assume-yes netcat-openbsd"
 docker start client-netcat
@@ -14,7 +14,7 @@ RESPUESTA=$(docker exec client-netcat bash -c "echo $MENSAJE | netcat $DIRECCION
 docker stop client-netcat
 docker rm client-netcat
 
-make docker-compose-down
+# make docker-compose-down
 
 if [ "$RESPUESTA" = "$MENSAJE" ];
 then
